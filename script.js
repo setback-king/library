8ef40172f8add1ec9fca2fd46be005447d0a92b8
setback-king <90795553+setback-king@users.noreply.github.com>
2022-05-10 08:42:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const check2 = document.getElementById('read2');
 const formInput= document.getElementById('formInput');
 const btn = document.querySelector('#readInfo');
 
-console.log("git updated")
+
 // adds book to array 
 function addBookToLibrary() {
   let newBook = new Book(title1.value, author1.value, pages1.value, checkedValue);
@@ -192,6 +192,7 @@ if ( window.history.replaceState ) {
 // returns value regarding local storage compatability 
 if (storageAvailable('localStorage')) {
     console.log("yes")
+    console.log("git updated")
   }
   else {
    console.log('no')
